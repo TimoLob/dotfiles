3a29f737270e1c23e6351269f3e601f363e67684
Timo Lobitz <timo.lobitz@gmail.com>
2024-04-21 18:26:57 +0200

    Added --noconfirm to package installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 cd ~
 git clone https://github.com/TimoLob/dotfiles.git --recurse-submodules
 
-yay -S kitty zsh stow ttf-meslo-nerd-font-powerlevel10k zsh-theme-powerlevel10k-git ripgrep zoxide neovim-git
+yay -S kitty zsh stow ttf-meslo-nerd-font-powerlevel10k zsh-theme-powerlevel10k-git ripgrep zoxide neovim-git --noconfirm
 
 cd dotfiles
 stow .
