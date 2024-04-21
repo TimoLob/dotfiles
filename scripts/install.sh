76aa027efe650cc2884dbf720b9982ffb09d99e3
Timo Lobitz <timo.lobitz@gmail.com>
2024-04-21 18:38:38 +0200

    Move existing files out of the way (but keep as backup) for stow

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,15 @@ git clone https://github.com/TimoLob/dotfiles.git --recurse-submodules
 
 yay -S kitty zsh stow ttf-meslo-nerd-font-powerlevel10k zsh-theme-powerlevel10k-git ripgrep zoxide neovim-git --noconfirm
 
+
+mv ~/.config/kitty ~/.config/kitty_bak
+mv ~/.config/i3 ~/.config/i3_bak
+mv ~/.config/nvim ~/.config/nvim_bak
+mv ~/.zshrc ~/.zshrc_bak
+mv ~/.backgrounds ~/.backgrounds_bak
+
 cd dotfiles
 stow .
 cd scripts
-source set_bg.sh
+source ./set_bg.sh
 chsh -s $(which zsh)
